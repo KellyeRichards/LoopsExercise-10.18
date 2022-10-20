e021d331c0e4a3287bb480921f248942d47545fd
Kellye Richards <kellye.whitfill@gmail.com>
2022-10-20 16:12:27 -0500

    updated a couple questions

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ for (i = 1; i < 8; i++){
     console.log(i);
 }
 
+let b = `=`.repeat(25);
+console.log(b);
+
 //2. Use a For Loop to count from 5 to 25 by 4 (Ex: 5, 9, 13, etc.) and have each number display in the console/terminal (You will need a console log for this)
-for (i = 5; i < 26; i +=4) {
+for (i = 5; i <= 25; i +=4) {
     console.log(i);
 }
 
@@ -16,8 +19,8 @@ const wizards =[
 ];
 
 //3b. Use a For Of Loop to have each name display in the console/terminal (You will need a console log for this)
-for (i = 0; i < wizards.length; i++) {
-    console.log(wizards[i]);
+for (friends of wizards) {
+    console.log(friends);
 }
 
 //4a. Create a let variable called harryPotterMovies and set the value to 0 (That's a zero not O).
@@ -50,19 +53,19 @@ for (house of hogwartsHouses){
 
 //6a. Create a const variable named quote and set the value of the variable to the following array items "Yer", "A", "Wizard", "Harry".
 const quote = [
-    `Yer `,
-    `A `,
-    `Wizzard `,
+    `Yer`,
+    `A`,
+    `Wizzard`,
     `Harry`
 ];
 
 //6b. Using a loop (You can decide which one you want to use) and the QUOTE array have the message "Yer A Wizard Harry" display on ONE line in the console/terminal (NOTE: Must have spaces between the words when it is displayed. Also, You will need a console log for this)(HINT: You will need to create another variable)
 let message = ``;
-for (i = 0; i < quote.length; i++) {
-    message = message + quote[i];
+for (i = 0; i < quote.length; i++){
+    message += ` ${quote[i]}`;
 }
+message = message.trim();
 console.log(message);
-
 
 //7. Create a For Loop that displays numbers 1 to 25 (AKA Counts from 1 to 25) in the terminal/console when the file is run. However, for numbers that are multiples of 3 (3, 6, 9, etc.) have the string value of "Expecto" display instead of the number. Also, for numbers that are multiples of 5 (5, 10, 15, etc.) have the string value of "Patronum" display instead of the number. Finally, for numbers that are multiples of 3 & 5 have the string value of "Expecto Patronum" display instead of the number.
 for (i = 1; i <= 25; i++) {
